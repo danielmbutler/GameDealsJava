c6f843439a1484ff3270f4e20852b5a045ddb704
Daniel Butler <danielmbutler@hotmail.co.uk>
2021-08-18 17:24:52 +0100

    implement api call and test with volley

--- app/src/main/java/com/dbtechprojects/gamedealsjava/repository/RepositoryImpl.java
+++ app/src/main/java/com/dbtechprojects/gamedealsjava/repository/RepositoryImpl.java
@@ -1,22 +1,36 @@
 package com.dbtechprojects.gamedealsjava.repository;
 
+import static com.dbtechprojects.gamedealsjava.utils.Constants.BASE_URL;
+import static com.dbtechprojects.gamedealsjava.utils.Constants.SEARCH_URL;
+
+import android.app.Application;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.dbtechprojects.gamedealsjava.api.ApiClient;
 import com.dbtechprojects.gamedealsjava.models.Game;
+import com.dbtechprojects.gamedealsjava.utils.MyApplication;
+
+import org.json.JSONArray;
 
 import java.util.List;
 
+
 // singleton design pattern https://www.journaldev.com/1377/java-singleton-design-pattern-best-practices-examples
 
-public class RepositoryImpl implements DefaultRepository{
+public class RepositoryImpl implements DefaultRepository {
 
     // singleton declaration
     private static final RepositoryImpl instance = new RepositoryImpl();
 
     //private constructor to avoid client applications to use constructor
-    private RepositoryImpl(){}
+    private RepositoryImpl() {
+    }
 
-    public static RepositoryImpl getInstance(){
+    public static RepositoryImpl getInstance() {
         return instance;
     }
 
@@ -24,6 +38,22 @@ public class RepositoryImpl implements DefaultRepository{
     @Override
     public List<Game> getGames(String query) {
         Log.d("repository", "getGames method called");
+        String requestUrl = BASE_URL + SEARCH_URL + query;
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                requestUrl,
+
+                response -> {
+                    Log.d("repository", response.toString());
+                },
+                error -> {
+                    Log.d("repository", error.getMessage());
+                }
+        );
+
+        // Access the RequestQueue through singleton class.
+        ApiClient.getInstance(MyApplication.getAppContext()).addToRequestQueue(jsonArrayRequest);
+
         return null;
     }
 
---
+++ app/src/main/java/com/dbtechprojects/gamedealsjava/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.dbtechprojects.gamedealsjava.utils;
+
+public class Constants {
+    public static final String BASE_URL = "https://www.cheapshark.com/api/1.0/";
+    public static final String SEARCH_URL = "games?title=";
+}
